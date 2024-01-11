eb8d890c6bd0cc04feddfd930f70c5504fcc037b
KhairuzzamanCP <mdkhariuzzaman@gmail.com>
2024-01-11 15:00:43 +0600

    server

--- accounts/views.py
+++ accounts/views.py
@@ -13,7 +13,7 @@ from transactions.views import send_email
 class UserRegisterView(FormView):
     template_name = 'accounts/register.html'
     form_class = UserRegistrationForm
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('login')
 
     def form_valid(self, form):
         user = form.save()
@@ -22,7 +22,7 @@ class UserRegisterView(FormView):
         return super().form_valid(form)
     
     def form_invalid(self, form):
-        messages.success(self.request, 'Account Created Successfully.')
+        messages.error(self.request, 'somethings Woring please Try again')
         return super().form_invalid(form)
 
 class UserLoginView(LoginView):
